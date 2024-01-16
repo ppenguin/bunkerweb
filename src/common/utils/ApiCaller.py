785d3f21f8bc6f065c54fbe98f6b9344f5fbd777
ppenguin <hieronymusv@gmail.com>
2024-01-16 14:55:02 +0100

    Add override to Swarm,k8s and linux modes too

--- src/common/utils/ApiCaller.py
+++ src/common/utils/ApiCaller.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from copy import deepcopy
 from io import BytesIO
 from os import getenv, sep
 from os.path import join
@@ -9,7 +7,6 @@ from sys import path as sys_path
 from tarfile import open as tar_open
 from typing import Any, Dict, List, Literal, Optional, Tuple, Union
 
-# Update system path for dependencies
 for deps_path in [join(sep, "usr", "share", "bunkerweb", *paths) for paths in (("deps", "python"), ("utils",))]:
     if deps_path not in sys_path:
         sys_path.append(deps_path)
@@ -17,11 +14,90 @@ for deps_path in [join(sep, "usr", "share", "bunkerweb", *paths) for paths in ((
 from API import API  # type: ignore
 from logger import setup_logger
 
+from docker import DockerClient
+from kubernetes import client as kube_client, config
+
 
 class ApiCaller:
     def __init__(self, apis: Optional[List[API]] = None):
-        self.apis = apis or []
-        self.__logger = setup_logger("Api", getenv("CUSTOM_LOG_LEVEL", getenv("LOG_LEVEL", "INFO")))
+        self.__apis = apis or []
+        self.__logger = setup_logger("Api", getenv("LOG_LEVEL", "INFO"))
+
+    @property
+    def apis(self) -> List[API]:
+        return self.__apis
+
+    @apis.setter
+    def apis(self, apis: List[API]):
+        self.__apis = apis
+
+    def auto_setup(self, bw_integration: Optional[str] = None):
+        self.__apis.clear()
+        if bw_integration is None:
+            if getenv("KUBERNETES_MODE", "no") == "yes":
+                bw_integration = "Kubernetes"
+            elif getenv("SWARM_MODE", "no") == "yes":
+                bw_integration = "Swarm"
+
+        if bw_integration == "Kubernetes":
+            config.load_incluster_config()
+            corev1 = kube_client.CoreV1Api()
+            for pod in corev1.list_pod_for_all_namespaces(watch=False).items:
+                if pod.metadata.annotations is not None and "bunkerweb.io/INSTANCE" in pod.metadata.annotations:
+                    api_http_port = None
+                    api_server_name = None
+
+                    for pod_env in pod.spec.containers[0].env:
+                        if pod_env.name == "API_HTTP_PORT":
+                            api_http_port = pod_env.value or "5000"
+                        elif pod_env.name == "API_SERVER_NAME":
+                            api_server_name = pod_env.value or "bwapi"
+
+                    self.__apis.append(
+                        API(
+                            f"http://{getenv("API_HTTP_HOST", pod.status.pod_ip)}:{api_http_port or getenv('API_HTTP_PORT', '5000')}",
+                            host=api_server_name or getenv("API_SERVER_NAME", "bwapi"),
+                        )
+                    )
+        else:
+            docker_client = DockerClient(base_url=getenv("DOCKER_HOST", "unix:///var/run/docker.sock"))
+
+            if bw_integration == "Swarm":
+                for instance in docker_client.services.list(filters={"label": "bunkerweb.INSTANCE"}):
+                    api_http_port = None
+                    api_server_name = None
+
+                    for var in instance.attrs["Spec"]["TaskTemplate"]["ContainerSpec"]["Env"]:
+                        if var.startswith("API_HTTP_PORT="):
+                            api_http_port = var.replace("API_HTTP_PORT=", "", 1)
+                        elif var.startswith("API_SERVER_NAME="):
+                            api_server_name = var.replace("API_SERVER_NAME=", "", 1)
+
+                    for task in instance.tasks():
+                        self.__apis.append(
+                            API(
+                                f"http://{getenv("API_HTTP_HOST", ".".join([ instance.name, task['NodeID'], task['ID'] ]))}:{api_http_port or getenv('API_HTTP_PORT', '5000')}",
+                                host=api_server_name or getenv("API_SERVER_NAME", "bwapi"),
+                            )
+                        )
+                return
+
+            for instance in docker_client.containers.list(filters={"label": "bunkerweb.INSTANCE"}):
+                api_http_port = None
+                api_server_name = None
+
+                for var in instance.attrs["Config"]["Env"]:
+                    if var.startswith("API_HTTP_PORT="):
+                        api_http_port = var.replace("API_HTTP_PORT=", "", 1)
+                    elif var.startswith("API_SERVER_NAME="):
+                        api_server_name = var.replace("API_SERVER_NAME=", "", 1)
+
+                self.__apis.append(
+                    API(
+                        f"http://{getenv("API_HTTP_HOST", instance.name)}:{api_http_port or getenv('API_HTTP_PORT', '5000')}",
+                        host=api_server_name or getenv("API_SERVER_NAME", "bwapi"),
+                    )
+                )
 
     def send_to_apis(
         self,
@@ -30,48 +106,49 @@ class ApiCaller:
         files: Optional[Dict[str, BytesIO]] = None,
         data: Optional[Dict[str, Any]] = None,
         response: bool = False,
-    ) -> Tuple[bool, Optional[Dict[str, Any]]]:
-        def send_request(api, files):
-            sent, err, status, resp = api.request(method, url, files=files, data=data)
-            return api, sent, err, status, resp
-
+    ) -> Tuple[bool, Tuple[bool, Optional[Dict[str, Any]]]]:
         ret = True
-        url = url.lstrip("/")
-        responses = {} if response else None
-
-        if files:
-            for buffer in files.values():
-                buffer.seek(0, 0)  # Ensure the file pointer is at the beginning
-
-        with ThreadPoolExecutor() as executor:
-            future_to_api = {executor.submit(send_request, api, deepcopy(files) if files else None): api for api in self.apis}
-            for future in as_completed(future_to_api):
-                api = future_to_api[future]
-                try:
-                    api, sent, err, status, resp = future.result()
-                    if not sent:
-                        ret = False
-                        self.__logger.error(f"Can't send API request to {api.endpoint}{url} : {err}")
-                    else:
-                        if status != 200:
-                            ret = False
-                            self.__logger.error(f"Error while sending API request to {api.endpoint}{url} : status = {status}, msg = {resp.get('msg')}")
-                        else:
-                            self.__logger.info(f"Successfully sent API request to {api.endpoint}{url}")
-
-                        if resp and response:
-                            instance = api.endpoint.replace("http://", "").split(":")[0]
-                            responses[instance] = resp if isinstance(resp, dict) else resp.json()
-                except Exception as exc:
+        url = url if not url.startswith("/") else url[1:]
+        responses = {}
+        for api in self.__apis:
+            if files is not None:
+                for buffer in files.values():
+                    buffer.seek(0, 0)
+            sent, err, status, resp = api.request(method, url, files=files, data=data)
+            if not sent:
+                ret = False
+                self.__logger.error(
+                    f"Can't send API request to {api.endpoint}{url} : {err}",
+                )
+            else:
+                if status != 200:
                     ret = False
-                    self.__logger.error(f"API request generated an exception: {exc}")
+                    self.__logger.error(
+                        f"Error while sending API request to {api.endpoint}{url} : status = {resp['status']}, msg = {resp['msg']}",
+                    )
+                else:
+                    self.__logger.info(
+                        f"Successfully sent API request to {api.endpoint}{url}",
+                    )
 
-        return ret, responses
+                    if response:
+                        instance = api.endpoint.replace("http://", "").split(":")[0]
+                        if isinstance(resp, dict):
+                            responses[instance] = resp
+                        else:
+                            responses[instance] = resp.json()
+
+        if response and responses:
+            return ret, responses
+        return ret
 
     def send_files(self, path: str, url: str) -> bool:
+        ret = True
         with BytesIO() as tgz:
             with tar_open(mode="w:gz", fileobj=tgz, dereference=True, compresslevel=3) as tf:
                 tf.add(path, arcname=".")
             tgz.seek(0, 0)
             files = {"archive.tar.gz": tgz}
-            return self.send_to_apis("POST", url, files=files)[0]
+            if not self.send_to_apis("POST", url, files=files):
+                ret = False
+        return ret
